4680cd7d51bc9e2f0f15eb99ceb34ab55ffab979
unknown <SangHe>
2023-04-03 01:31:45 +0900

    upload

---
+++ js/clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector('#clock')
+
+function getClock(){
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, '0')
+    const minute = String(date.getMinutes()).padStart(2, '0')
+    const sec = String(date.getSeconds()).padStart(2, '0')
+    clock.innerText = `${hour}:${minute}:${sec}`
+}
+getClock();
+
+setInterval(getClock, 1000)  // 5초마다 반복호출
+//setTimeout(sayHello, 5000)  // 5초뒤에 호출
---
+++ js/login.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");  //documnert가 아니라 loginForm에서 찾을수있다.
+const greeting = document.querySelector("#geeting")
+
+const USERNAME = "username"
+function onLoginSubmitClick(event){
+  event.preventDefault();  //이벤트에 기본동작을 막는다
+  loginForm.classList.add("hiden")
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME, username)
+  painGreeting(username)
+}
+
+function painGreeting(username){
+  greeting.innerText = "Wellcome Calender Scaduel, " + username
+}
+
+const savedUsername = localStorage.getItem(USERNAME)
+
+if(savedUsername === null){
+  loginForm.classList.remove("hiden");
+  loginForm.addEventListener("submit", onLoginSubmitClick);
+}else{
+  painGreeting(savedUsername)
+}
+
+window.addEventListener('load',()=>{
+    if(savedUsername){
+        localStorage.removeItem(USERNAME)
+    }
+})
